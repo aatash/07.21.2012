11464877f7710661c17838b80014e91fd1d804d9
Edward Wang <edward.auc@gmail.com>
2012-07-22 05:30:02 -0700

    Updated sessions and login logic

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+require_once('function.php');
+$test = DB::queryFirstRow("SELECT * from users WHERE username = %s", $_POST['username']);
+if($test) {
+	$_SESSION['username'] = $_POST['username'];
+	}
+else {
+	echo "invalid password.";
+	}
+	
